27b0d6a0ac2fe950b1aa89f16dc4f71afbdfb6f7
riveralights <riveralights@gmail.com>
2021-08-03 14:17:27 +0700

    Show books from dummy data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Beranda from "./components/Beranda";
 import ManajemenBuku from "./components/ManajemenBuku";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [books, setBooks] = useState([
+    {
+      _id: 1,
+      judul: "Laskar Pelangi",
+      pengarang: "Andrea Hirata",
+      harga: 80000,
+      stok: 6
+    },
+    {
+      _id: 2,
+      judul: "Bumi",
+      pengarang: "Tere Liye",
+      harga: 86000,
+      stok: 4
+    }
+  ]);
+
   return (
     <div>
       <BrowserRouter>
@@ -13,7 +31,7 @@ function App() {
             <Beranda />
           </Route>
           <Route path="/">
-            <ManajemenBuku />
+            <ManajemenBuku bookList={books} />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -1,10 +1,42 @@
-function ManajemenBuku(){
+function ManajemenBuku({bookList}){
+    console.log(bookList);
     return (
         <div className="container mt-3">
             <h1 className="text-center">Manajemen Buku</h1>
             <div id="formTambah"></div>
             <div id="formUbah"></div>
-            <div id="daftarBuku"></div>
+            <div id="daftarBuku">
+                <h2 className="mt-5">Daftar Buku</h2>
+                <hr />
+                <button className="btn btn-primary mb-4">Tambah Buku</button>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr className="text-center">
+                            <th width="5%">No.</th>
+                            <th>Judul</th>
+                            <th>Pengarang</th>
+                            <th>Harga</th>
+                            <th>Stok</th>
+                            <th width="15%">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bookList.map((book, index) => (
+                            <tr key={index}>
+                                <td className="text-center align-middle">{index+1}</td>
+                                <td className="align-middle">{book.judul}</td>
+                                <td className="align-middle">{book.pengarang}</td>
+                                <td className="text-center align-middle">{book.harga}</td>
+                                <td className="text-center align-middle">{book.stok}</td>
+                                <td className="text-center align-middle">
+                                    <button className="btn btn-warning me-2">Edit</button>
+                                    <button className="btn btn-danger">Hapus</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
